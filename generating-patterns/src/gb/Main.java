d9d0ef674758ee416a7f11166e5a78012c9e05bf
PSelyutin <pselyutin@inno.tech>
2022-06-14 09:00:04 +0400

    lesson03 Применение паттерна Builder

---
+++ generating-patterns/src/gb/Main.java
@@ -0,0 +1,31 @@
+package gb;
+
+import gb.builder.ClaimBuilder;
+import gb.builder.ServiceBuilder;
+import gb.builder.ServiceGroupBuilder;
+import gb.domain.Claim;
+import gb.domain.Service;
+import gb.domain.ServiceGroup;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ServiceGroup serviceGroup = new ServiceGroupBuilder()
+                .withId(1L)
+                .withName("Прием деларации 3-НДФЛ")
+                .build();
+        Service service = new ServiceBuilder()
+                .withServiceGroup(serviceGroup)
+                .withId(1L)
+                .withName("Прием деларации 3-НДФЛ  физических лиц")
+                .build();
+
+        Claim claim = new ClaimBuilder()
+                .withId(1L)
+                .withService(service)
+                .withCurrentDayNumber(1L)
+                .build();
+
+        System.out.println(claim.toString());
+    }
+}
